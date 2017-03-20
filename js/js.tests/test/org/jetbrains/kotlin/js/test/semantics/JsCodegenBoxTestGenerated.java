884b745e465208e55e5bfcd5c93cc9c18a713f75
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2017-03-20 18:34:08 +0300

    Fix inlining of tail call suspend function
    When inlining tail-call suspend function to regular
    suspend function in JS BE, don't forget to insert suspension point.
    See KT-16951

--- js/js.tests/test/org/jetbrains/kotlin/js/test/semantics/JsCodegenBoxTestGenerated.java
+++ js/js.tests/test/org/jetbrains/kotlin/js/test/semantics/JsCodegenBoxTestGenerated.java
@@ -6292,6 +6292,12 @@ public class JsCodegenBoxTestGenerated extends AbstractJsCodegenBoxTest {
                 String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/codegen/box/coroutines/tailOperations/suspendWithWhen.kt");
                 doTest(fileName);
             }
+
+            @TestMetadata("tailInlining.kt")
+            public void testTailInlining() throws Exception {
+                String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/codegen/box/coroutines/tailOperations/tailInlining.kt");
+                doTest(fileName);
+            }
         }
 
         @TestMetadata("compiler/testData/codegen/box/coroutines/unitTypeReturn")
