e6f2f280e3c1e76653c149e077213b0463af9836
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2017-03-22 18:53:30 +0300

    Revert "Replace removed method in LibraryPresentationProviders"
    This reverts commit e57b3651c238ffc5f778b7346da890e69649fadc.

--- idea/src/org/jetbrains/kotlin/idea/framework/JSLibraryStdPresentationProvider.java
+++ idea/src/org/jetbrains/kotlin/idea/framework/JSLibraryStdPresentationProvider.java
@@ -20,6 +20,7 @@ import com.intellij.framework.library.LibraryVersionProperties;
 import com.intellij.openapi.roots.OrderRootType;
 import com.intellij.openapi.roots.libraries.Library;
 import com.intellij.openapi.roots.libraries.LibraryPresentationProvider;
+import com.intellij.openapi.roots.libraries.LibraryProperties;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -40,7 +41,7 @@ public class JSLibraryStdPresentationProvider extends LibraryPresentationProvide
 
     @Nullable
     @Override
-    public Icon getIcon(@Nullable LibraryVersionProperties properties) {
+    public Icon getIcon(@Nullable LibraryProperties properties) {
         return KotlinIcons.SMALL_LOGO;
     }
 
